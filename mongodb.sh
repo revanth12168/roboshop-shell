bf9613fb4aa2ce1b352b9baf2ef388487b4c5887
revanth <revanthreddy12168@gmail.com>
2023-06-09 20:21:24 +0530

    Front end

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongodb.conf /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-sed -i -e 's|127.0.0.1|0.0.0.0|' mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
