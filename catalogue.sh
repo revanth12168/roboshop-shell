aea8069488fcd86cf39b6c065736c9ba88a47eca
revanth <revanthreddy12168@gmail.com>
2023-06-09 21:52:18 +0530

    Front end

---
+++ catalogue.sh
@@ -0,0 +1,36 @@
+echo -e "\e[36m<<<<configure npm >>>>>>>\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m <<<<<<install>>>>>>\e[0m"
+yum install nodejs -y
+
+echo -e "\e[36m <<<<<<Add user>>>>>>\e[0m"
+useradd roboshop
+
+echo -e "\e[36m <<<<<<create directory>>>>>>\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[36m <<<<<<source>>>>>>\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+
+echo -e "\e[36m <<<<<<source>>>>>>\e[0m"
+unzip /tmp/catalogue.zip
+echo -e "\e[36m <<<<<<dependencies>>>>>>\e[0m"
+npm install
+
+echo -e "\e[36m <<<<<<Create config files>>>>>>\e[0m"
+cp catalogue.conf /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m <<<<<<start service>>>>>>\e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
+echo -e "\e[36m <<<<<<copy Mongo DB>>>>>>\e[0m"
+cp mongodb.conf /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m <<<<<< Install mongodb client >>>>>>\e[0m"
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.revanthr.online </app/schema/c atalogue.js
