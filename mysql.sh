6adb5b1c290ba7d735716cb869ef6b966c3b6aae
revanth <revanthreddy12168@gmail.com>
2023-06-09 23:11:48 +0530

    Front end

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 yum module disable mysql -y
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
