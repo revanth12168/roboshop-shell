fb218c11b3e03eb74010a2722a14da26c208a3d9
revanth <revanthreddy12168@gmail.com>
2023-06-11 10:56:08 +0530

    Front end

---
+++ dispatch.sh
@@ -0,0 +1,28 @@
+echo -e "\e[36m <<<<< installing golang>>>>>\e[0m"
+yum install golang -y
+
+echo -e "\e[36m <<<<<<< adding user >>>>>>>\e[om"
+useradd roboshop
+
+echo -e "\e[36m <<<<<<< creating the directory >>>>>>>\e[om"
+mkdir /app
+
+echo -e "\e[36m <<<<<<< downloading the source >>>>>>>\e[om"
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+
+echo -e "\e[36m <<<<<<< unzip the source >>>>>>>\e[om"
+cd /app
+unzip /tmp/dispatch.zip
+
+echo -e "\e[36m <<<<<<< Downloading the dependencies >>>>>>>\e[om"
+go mod init dispatch
+go get
+go build
+
+echo -e "\e[36m <<<<<<< Creating the service and updating the file >>>>>>>\e[om"
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+
+echo -e "\e[36m <<<<<<< Starting the service >>>>>>>\e[om"
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl start dispatch
