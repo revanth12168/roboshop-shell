a68c7baebcc4f1b187ed6ed5b8e75aed85a098ba
revanth <revanthreddy12168@gmail.com>
2023-06-11 20:46:51 +0530

    Front end

--- common.sh
+++ common.sh
@@ -4,16 +4,16 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-print head() {
+print_head() {
   echo -e "\e[36m <<<<< $1 >>>>>\e[0m"
 }
 
-schema setup() {
+schema_setup() {
 
-  print head copy Mongo DB
+  print_head "copy Mongo DB"
   cp ${script_path}/mongodb.conf /etc/yum.repos.d/mongo.repo
 
-  print head Install mongodb client
+  print_head "Install mongodb client"
   yum install mongodb-org-shell -y
   mongo --host mongodb-dev.revanthr.online </app/schema/catalogue.js
 
@@ -21,36 +21,36 @@ schema setup() {
 
 fnc_nodejs() {
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   yum install nodejs -y
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   useradd ${app_user}
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   rm -rf /app
   mkdir /app
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   cd /app
   unzip /tmp/${component}.zip
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   npm install
 
-  print head "checking"
+  print_head "checking"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-  print head "Repo file as a rpm"
+  print_head "Repo file as a rpm"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
-  schema setup
+  schema_setup
 }
 
